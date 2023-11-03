09706adeb732f3750fe369d20886737047a500d6
Nagy Janos <105793197+NaJaBox@users.noreply.github.com>
2023-11-03 21:57:26 +0100

    modify Users components * add cards for users; * users can be sorted by age, country and education in ascending and descending order; * add some style;

--- components/Users/Users.jsx
+++ components/Users/Users.jsx
@@ -1,23 +1,25 @@
 "use client";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import React, { useState, useEffect } from "react";
 
 export const Users = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isAscending, setIsAscending] = useState(true);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch("/api/users"); // Replace with your API endpoint
+        const response = await fetch("/api/users");
         if (!response.ok) {
           throw new Error(
             `Failed to fetch users: ${response.status} - ${response.statusText}`
           );
+        } else {
+          const data = await response.json();
+          setUsers(data.users);
+          setLoading(false);
         }
-        const data = await response.json();
-        setUsers(data.users);
-        setLoading(false);
       } catch (error) {
         console.error("Error fetching users:", error);
         setLoading(false);
@@ -27,9 +29,62 @@ export const Users = () => {
     fetchUsers();
   }, []); // The empty dependency array ensures this effect runs only once
 
+  function calculateAge(birthday) {
+    const birthDate = new Date(birthday);
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthDate.getFullYear();
+    return age;
+  }
+
+  const handleSortByAge = () => {
+    const sortedUsers = [...users];
+    sortedUsers.sort((a, b) => {
+      const ageA = calculateAge(a.birthday);
+      const ageB = calculateAge(b.birthday);
+      const comparison = ageA - ageB;
+
+      // Toggle the order based on the current state
+      return isAscending ? comparison : -comparison;
+    });
+
+    // Toggle the sorting order for the next click
+    setIsAscending(!isAscending);
+    setUsers(sortedUsers);
+  };
+
+  const handleSortByCountry = () => {
+    const sortedUsers = [...users];
+    sortedUsers.sort((a, b) => {
+      const countryA = a.country;
+      const countryB = b.country;
+
+      // Use localeCompare for string comparison (case-insensitive)
+      return isAscending
+        ? countryA.localeCompare(countryB)
+        : countryB.localeCompare(countryA);
+    });
+
+    // Toggle the sorting order for the next click
+    setIsAscending(!isAscending);
+    setUsers(sortedUsers);
+  };
+
+  const handleSortByEducation = () => {
+    const sortedUsers = [...users];
+    sortedUsers.sort((a, b) => {
+      const educationA = a.education;
+      const educationB = b.education;
+      return isAscending
+        ? educationA.localeCompare(educationB)
+        : educationB.localeCompare(educationA);
+    });
+    setIsAscending(!isAscending);
+    setUsers(sortedUsers);
+  };
+
   return (
-    <div>
-      <div className={styles.navbar}>
+    <div className={styles.main}>
+      <section className={styles.navbar}>
         <a href="#home">
           <div className={styles.acmeLogo}>
             <div className={styles.logoTxt}>
@@ -42,19 +97,53 @@ export const Users = () => {
           <a href="/">Home</a>
           <a href="#">Login</a>
         </div>
+      </section>
+      <div className={styles.content}>
+        <h2>Users</h2>
+        <div className={styles.layout}>
+          <section className={styles.users}>
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <div className={styles.userContainer}>
+                {users.map((user, index) => (
+                  <div key={index} className={styles.user}>
+                    <div>
+                      {user.firstName} {user.lastName} Age:{" "}
+                      {calculateAge(user.birthday)}
+                    </div>
+                    <div>Birthday: {user.birthday}</div>
+                    <div>{user.country}</div>
+                    <div>{user.education}</div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </section>
+          <section className={styles.rightMenu}>
+            <aside>
+              <h2>Select Users By</h2>
+              <div className={styles.selectBtn}>
+                <button className={styles.ageBtn} onClick={handleSortByAge}>
+                  Age
+                </button>
+                <button
+                  className={styles.countryBtn}
+                  onClick={handleSortByCountry}
+                >
+                  Country
+                </button>
+                <button
+                  className={styles.educationBtn}
+                  onClick={handleSortByEducation}
+                >
+                  Education
+                </button>
+              </div>
+            </aside>
+          </section>
+        </div>
       </div>
-      <h2>Users</h2>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {users.map((user, index) => (
-            <li key={index}>
-              {user.firstName} {user.lastName}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
