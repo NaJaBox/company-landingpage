c68b1646da448c7d4ed024b041a2025ca40da40a
Nagy Janos <105793197+NaJaBox@users.noreply.github.com>
2024-04-29 23:53:58 +0200

    add spinner to improve UX

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
 import styles from "./page.module.css";
 import Hero from "./sections/hero/page";
-import Benefits from "./sections/benefits/page"
+import Benefits from "./sections/benefits/page";
 import Offer from "./sections/offer/page";
-import Steps from "./sections/CTA/page"
-import Footer from "./sections/footer/page"; 
+import Steps from "./sections/CTA/page";
+import Footer from "./sections/footer/page";
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <>
-        <Hero />
-        <Benefits/>
-        <Offer />
-        <Steps/>
-        <Footer/>
-      </>
-    </main>
+      <main className={styles.main}>
+        <>
+          <Hero />
+          <Benefits />
+          <Offer />
+          <Steps />
+          <Footer />
+        </>
+      </main>
   );
 }
